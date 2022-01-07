b2f504772e65c8ebfa89512e50bcd57f53585654
小明 <a1073506500@qq.com>
2022-01-07 23:55:46 +0800

    2022年1月7日 代码提交

---
+++ src/main/java/com/hongna/demo/array/Array.java
@@ -0,0 +1,18 @@
+package com.hongna.demo.array;
+
+/**
+ * 一个普通数组的各类操作实现
+ */
+public class Array {
+    //定义整型数组data保存数据
+    public int data[];
+    //定义数组长度
+    public int n;
+    //定义数组中实际个数
+    public int count;
+
+    //构造方法 定义数组大小
+    //根据索引，找到数组中的元素并返回
+    //插入元素：头部插入、尾部插入
+    //根据索引，删除数组中元素
+}
