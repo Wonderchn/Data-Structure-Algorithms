e3d66ee9d708859ab29422ae4f242ef375e89043
小明 <a1073506500@qq.com>
2022-01-08 17:47:41 +0800

    2022年1月8日 动态数组代码实现

--- src/main/java/com/hongna/demo/array/GenericArray.java
+++ src/main/java/com/hongna/demo/array/GenericArray.java
@@ -46,12 +46,13 @@ public class GenericArray<T> {
     }
     //获取对应index位置的元素
     public T get(int index) {
+        checkIndex(index);
         return  data[index];
     }
     //查看数组是否包含元素e
     public boolean contains(T e) {
         for (T i : data){
-            if (i == e){
+            if (i .equals(e) ){
                 return true;
             }
         }
@@ -69,12 +70,17 @@ public class GenericArray<T> {
 
         //在index位置，插入元素e，时间复杂度O(m+n)
     public void add(int index, T e) {
+        checkIndexForAdd(index);
+        // 如果当前元素个数等于数组容量，则将数组扩容为原来的2倍
+        if (size == data.length) {
+            resize(2 * data.length);
+        }
         //在此位置插入元素，所有元素往后移动一位
         for (int i = size - 1 ; i > index - 1 ; i--){
             data[i+1] = data[i];
         }
         data[index] = e;
-
+        size ++;
     }
     // 向数组头插入元素
     public void addFirst(T e) {
@@ -87,17 +93,71 @@ public class GenericArray<T> {
     }
     // 删除 index 位置的元素，并返回
     public T remove(int index) {
-
+        //0123
+        //123
+        T e = data[index];
+        for (int i = index ; i < size-1 ; i++){
+            data[i] = data[i+1];
+        }
+        size --;
+        data[size] = null;
+        // 缩容
+        if (size == data.length / 4 && data.length / 2 != 0) {
+            resize(data.length / 2);
+        }
+        return e;
     }
-                                    // 删除第一个元素
-//                                    public T removeFirst() {
-                                        // 删除末尾元素
-//                                        public T removeLast() {
+    // 删除第一个元素
+    public T removeFirst() {
+        return  remove(0);
+    }
+    // 删除末尾元素
+    public T removeLast() {
+        return  remove(size);
+    }
 
-                                        // 从数组中删除指定元素
-//                                        public void removeElement(T e) {
-                                            // 扩容方法，时间复杂度 O(n)
-//                                            private void resize(int capacity) {
-//                                        }
+    // 从数组中删除指定元素
+    public void removeElement(T e) {
+        int index = find(e);
+        if (index != -1) {
+            remove(index);
+        }
+    }
 
+    // 扩容方法，时间复杂度 O(n)
+    private void resize(int capacity) {
+        //假设我们现在新加元素是1.5倍来计算
+        T[] newData = (T[]) new Object[capacity];
+
+        for (int i = 0; i < size; i++) {
+            newData[i] = data[i];
+        }
+        data = newData;
+    }
+    private void checkIndex(int index) {
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("Add failed! Require index >=0 and index < size.");
+        }
+    }
+
+    private void checkIndexForAdd(int index) {
+        if(index < 0 || index > size) {
+            throw new IllegalArgumentException("remove failed! Require index >=0 and index <= size.");
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append(String.format("Array size = %d, capacity = %d \n", size, data.length));
+        builder.append('[');
+        for (int i = 0; i < size; i++) {
+            builder.append(data[i]);
+            if (i != size - 1) {
+                builder.append(", ");
+            }
+        }
+        builder.append(']');
+        return builder.toString();
+    }
 }
