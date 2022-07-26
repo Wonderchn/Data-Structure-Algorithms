8c816842312ee4e3d99becc5c31137f3c7d60c79
wondercc <a1073506500@qq.com>
2022-07-26 14:19:07 +0800

    2022年7月26日 代码提交

--- src/main/java/com/hongna/demo/bsearch_16/Bsearch.java
+++ src/main/java/com/hongna/demo/bsearch_16/Bsearch.java
@@ -16,7 +16,7 @@ public class Bsearch {
         int high = n - 1;
 
         while (low <= high) {
-            int mid = low + (high + low << 2);
+            int mid = low + (high + low >> 1);
             if (a[mid] == value) {
                 return mid;
             } else if (a[mid] > value) {
@@ -41,7 +41,7 @@ public class Bsearch {
         int low = 0;
         int high = n - 1;
         while (low <= high) {
-            int mid = low + (high + low << 2);
+            int mid = low + (high + low >> 1);
             if (a[mid] > value) {
                 high = mid - 1;
             } else if (a[mid] == value) {
@@ -55,5 +55,88 @@ public class Bsearch {
             }
         }
         return -1;
+    }
+
+
+    /**
+     * 变体二：查找最后一个值等于给定值的元素
+     *
+     * @param a
+     * @param n
+     * @param value
+     * @return
+     */
+    public int bsearch_2(int[] a, int n, int value) {
+        int low = 0;
+        int high = n - 1;
+        while (low <= high) {
+            int mid = low + (high + low >> 1);
+            if (a[mid] < value) {
+                low = mid + 1;
+            } else if (a[mid] == value) {
+                if (mid == a.length - 1 || a[mid + 1] != value) {
+                    return mid;
+                } else {
+                    low = mid + 1;
+                }
+            } else if (a[mid] < value) {
+                high = mid - 1;
+
+            }
+
+        }
+        return -1;
+    }
+
+
+    /**
+     * 变体三：查找第一个大于等于给定值的元素
+     *
+     * @param a
+     * @param n
+     * @param value
+     * @return
+     */
+    public int bsearch_3(int[] a, int n, int value) {
+        int low = 0;
+        int high = n - 1;
+        while (low <= high) {
+            int mid = low + (low + high >> 1);
+            if (a[mid] >= value) {
+                if ((mid == 0) || (a[mid - 1] < value)) {
+                    return mid;
+                } else {
+                    high = mid - 1;
+                }
+            } else {
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+
+    /**
+     *
+     * 变体四：查找最后一个小于等于给定值的元素
+     * @param a
+     * @param n
+     * @param value
+     * @return
+     */
+    public int bsearch_4(int[] a, int n, int value) {
+        int low = 0;
+        int high = n - 1;
+        while (low <= high) {
+            int mid = low + ((high + low) >> 1);
+            if (a[mid] > value) {
+                high = mid - 1;
+            } else {
+                if ((mid == n - 1) || (a[mid + 1] > value)) return mid;
+                else low = mid + 1;
+            }
+        }
+        return -1;
+
     }
 }
